4ba0b4d3db874d776445101abbe8441cdb65bd54
Kevin Grant <anmoron11@gmail.com>
2024-06-03 13:12:09 -0400

    Updated main layout

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,14 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
+import { Route, Routes } from "react-router-dom";
+import DeckList from "../Deck/DeckList";
+import { listDecks } from "../utils/api";
+import DeckPage from "../Deck/DeckPage";
+import DeckStudyPage from "../Deck/DeckStudyPage";
 
 function Layout() {
+  /**
+   * @TODO:
+   * 1. Mess around with the API to read all of the data.
+   * 2. Pass down the data via props.
+   */
+
+  const [decks, setDecks] = useState(null);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    async function loadData() {
+      const signal = abortController.signal;
+      const data = await listDecks(signal);
+
+      setDecks(data);
+    }
+
+    loadData();
+
+    // return abortController.abort();
+  }, []);
+
   return (
     <div>
       <Header />
       <div className="container">
         {/* TODO: Implement the screen starting here */}
-        <NotFound />
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<DeckList decks={decks} />} />
+          {/* Deck Routes */}
+          <Route path="/decks">
+            <Route path=":deckId" element={<DeckPage />} />
+            <Route path=":deckId/study" element={<DeckStudyPage />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
     </div>
   );
