bcb14f2d3c4edc0968f79c59eb1006864273fc7d
Kevin Grant <anmoron11@gmail.com>
2024-06-05 16:34:39 -0400

    Created card component.

---
+++ src/Cards/Card.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+function Card({ card, deckId, deleteCard, id }) {
+
+  return (
+    <div className="card mb-3">
+      <div className="card-body">
+        <div className="d-flex justify-content-between">
+          <div>
+            {card.front} {card.id}
+          </div>
+          <div>{card.back}</div>
+        </div>
+        <div className="d-flex justify-content-end mt-2">
+          <Link
+            className="btn btn-secondary mr-2"
+            to={`/decks/${deckId}/cards/${card.id}/edit`}
+          >
+            Edit
+          </Link>
+          <button className="btn btn-danger" onClick={() => deleteCard(id)}>Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
