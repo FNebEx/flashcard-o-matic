e2d610f6deec1cbbc6e60f87145f5af001b7199e
Kevin G. from laptop <anmoron11@gmail.com>
2024-06-06 13:40:28 -0400

    Created component

---
+++ src/Breadcrumbs.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Breadcrumbs({deck, pageTitle}) {
+  /**
+   * There is probably a more elegant way to make this using the useLocation hook.
+   */
+
+  return <div>
+    <Link to={'/'}>Home</Link> {deck && <span> / {deck.name}</span>} {pageTitle && <span className="text-muted"> / {pageTitle}</span>}
+  </div>;
+}
+
+export default Breadcrumbs;
