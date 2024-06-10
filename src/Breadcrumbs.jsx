85c40f69bd1074dbd6386b2de5112e897c170975
Kevin Grant <anmoron11@gmail.com>
2024-06-10 09:57:02 -0400

    Breadcrumbs

--- src/Breadcrumbs.jsx
+++ src/Breadcrumbs.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 
-function Breadcrumbs({deck, pageTitle}) {
+function Breadcrumbs({ deck, pageTitle }) {
   /**
    * There is probably a more elegant way to make this using the useLocation hook.
    */
 
-  return <div>
-    <Link to={'/'}>Home</Link> {deck && <span> / {deck.name}</span>} {pageTitle && <span className="text-muted"> / {pageTitle}</span>}
-  </div>;
+  return (
+    <div>
+      <Link to={"/"}>Home</Link> {deck && <span> / {deck.name}</span>}{" "}
+      {pageTitle && <span className="text-muted"> / {pageTitle}</span>}
+    </div>
+  );
 }
 
 export default Breadcrumbs;
